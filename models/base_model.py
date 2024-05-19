4d04d9e2d1028e9c5a569f0f41ec777e64ec6c8b
juniorrafiu7 <juniorrafiu7@gmail.com>
2024-05-19 21:24:38 +0000

    hard

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python3
 from uuid import uuid4
 from datetime import datetime
 class BaseModel():
-    def __init__(self, id, created_at, updated_at):
+    def __init__(self):
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
