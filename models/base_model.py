bf6e2fcf6fddd02f0f02ddeaebd301dc6b8da2ca
juniorrafiu7 <juniorrafiu7@gmail.com>
2024-05-20 14:54:48 +0000

    hard

--- models/base_model.py
+++ models/base_model.py
@@ -4,10 +4,25 @@ from datetime import datetime
 
 
 class BaseModel():
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    """
+    Defines all common attributes/methods for other classes
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes a new BaseModel
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+                    setattr(self, key, value)
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
