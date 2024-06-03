d25fa8197f734997c756f7cab6e91a339c85e517
juniorrafiu7 <juniorrafiu7@gmail.com>
2024-06-03 02:22:57 +0000

    hard

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from models import storage
 from models.base_model import BaseModel
 
@@ -14,29 +13,4 @@ my_model.name = "My_First_Model"
 my_model.my_number = 89
 my_model.save()
 print(my_model)
-=======
 
-from models.base_model import BaseModel
-
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-print(my_model.id)
-print(my_model)
-print(type(my_model.created_at))
-print("--")
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
-
-print("--")
-my_new_model = BaseModel(**my_model_json)
-print(my_new_model.id)
-print(my_new_model)
-print(type(my_new_model.created_at))
-
-print("--")
-print(my_model is my_new_model)
->>>>>>> d5660df596e027d58e0223329a7d10470f3992ce
