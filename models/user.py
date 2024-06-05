c52ec472ff32ee5ad72e6918bc3667505f3a79bb
juniorrafiu7 <juniorrafiu7@gmail.com>
2024-06-05 13:59:21 +0000

    hard

--- models/user.py
+++ models/user.py
@@ -1,8 +1,20 @@
+"""
+the module of User.
+Inherits from BaseModel.
+"""
 from models.base_model import BaseModel
+
+
 class User(BaseModel):
+     """
+    Represent a User.
+    Attributes:
+        email (str): email.
+        password (str): password.
+        first_name (str): first name.
+        last_name (str): last name.
+    """
     email = ''
     password = ''
     first_name = ''
     last_name = ''
-
-    
