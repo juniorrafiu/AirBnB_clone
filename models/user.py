790e24388182dc83a8494686ab4081c802b2d1a5
juniorrafiu7 <juniorrafiu7@gmail.com>
2024-06-05 16:06:46 +0000

    hard

--- models/user.py
+++ models/user.py
@@ -20,6 +20,4 @@ class User(BaseModel):
     first_name = ''
     last_name = ''
 
-    def __init__(self, *args, **kwargs):
-        """Initialize a new User"""
-        super().__init__(*args, **kwargs)
+    
